c636f9271b1eca1801efc3136147c5dd7a82c86f
Seungmin Yang <tmdals099@gmail.com>
2021-02-07 17:24:15 +0900

    Added positioning

--- app/src/main/java/com/evandefd/logiccircuitsimulator/DummyLogicCircuit.kt
+++ app/src/main/java/com/evandefd/logiccircuitsimulator/DummyLogicCircuit.kt
@@ -5,13 +5,15 @@ import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
 import com.evandefd.logiccircuitsimulator.view.component.LogicCircuitComponent
 
-class DummyLogicCircuit(override val context: Context, override val tag: String) : LogicCircuitComponent(context, tag) {
+class DummyLogicCircuit(override val context: Context, override val tag: String) :
+    LogicCircuitComponent(context, tag) {
     override val name: String = "Dummy"
     override val actualWidth: Float = 8f
     override val actualHeight: Float = 2f
-    override val image: Drawable = ContextCompat.getDrawable(context, R.drawable.ic_launcher_background)!!
+    override val image: Drawable =
+        ContextCompat.getDrawable(context, R.drawable.ic_launcher_background)!!
     override val contacts: List<Contact> = listOf(
-        Contact(1, "In", Contact.GENDER_FEMALE, 0.5f, 0.5f, 1f),
-        Contact(2, "Out", Contact.GENDER_FEMALE, 0.5f, 7.5f, 1f)
+        Contact(1, "In", Contact.GENDER_FEMALE, 0.5f, 1f),
+        Contact(2, "Out", Contact.GENDER_FEMALE, 7.5f, 1f)
     )
 }
--- app/src/main/java/com/evandefd/logiccircuitsimulator/view/component/LogicCircuitWire.kt
+++ app/src/main/java/com/evandefd/logiccircuitsimulator/view/component/LogicCircuitWire.kt
@@ -1,7 +1,6 @@
 package com.evandefd.logiccircuitsimulator.view.component
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.*
 
 open class LogicCircuitWire(
     startNickname: String = "wire_start",
@@ -12,7 +11,35 @@ open class LogicCircuitWire(
         Contact(1, endNickname, Contact.GENDER_UNSPECIFIED, Float.NaN, Float.NaN)
     )
 
-    fun makeBitmap() {
-        
+    fun makeBitmap(ltrb : Array<Float>) : Bitmap? {
+        if(contacts[0].contact == null || contacts[1].contact == null) {
+            return null
+        } else {
+            val x1 = contacts[0].contact!!.actualPositionX
+            val x2 = contacts[1].contact!!.actualPositionX
+            val y1 = contacts[0].contact!!.actualPositionY
+            val y2 = contacts[1].contact!!.actualPositionY
+
+            if(x1 > x2) {
+                ltrb[0] = x2
+                ltrb[2] = x1
+            } else {
+                ltrb[0] = x1
+                ltrb[2] = x2
+            }
+
+            if(y1 > y2) {
+                ltrb[1] = y2
+                ltrb[3] = y1
+            } else {
+                ltrb[1] = y1
+                ltrb[3] = y2
+            }
+
+            val bitmap = Bitmap.createBitmap((ltrb[2] - ltrb[0]).toInt(), (ltrb[3] - ltrb[1]).toInt(), Bitmap.Config.ARGB_8888)
+            
+
+            return bitmap
+        }
     }
 }
